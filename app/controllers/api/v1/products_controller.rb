de04994d3671700e1eb208deb3a3e82ca3c2bc1f
EC2 Default User <ec2-user@ip-172-31-11-98.eu-central-1.compute.internal>
2022-07-14 11:31:46 +0000

    code refactor

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,12 +2,15 @@ module Api
     module V1
         class ProductsController < ApplicationController
           before_action :set_product, except: %i[create index]
+          
           def index
             render json: { data: ActiveModel::SerializableResource.new(Product.all, each_serializer: ProductSerializer) }
           end
+          
           def show
             render json: ProductSerializer.new(@product).to_h
           end
+          
           def create
             @product = Product.new(product_params)
             if @product.save
@@ -16,6 +19,7 @@ module Api
               render json: { errors: @product.errors.to_s }, status: :unprocessable_entity
             end
           end
+          
           def update
             if @product.update(product_params)
               render json: @product
@@ -23,6 +27,7 @@ module Api
               render json: { errors: @product.errors.to_s }, status: :unprocessable_entity
             end
           end
+          
           def destroy
             if @product.destroy
               render json: @product
@@ -30,10 +35,13 @@ module Api
               render json: { errors: @product.errors.to_s }, status: :unprocessable_entity
             end
           end
+          
           private
+          
           def product_params
             params.require(:product).permit(%i[name quantity])
           end
+          
           def set_product
             @product = Product.find(params[:id])
           end

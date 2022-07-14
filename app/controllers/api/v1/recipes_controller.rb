60b40166ca60ca243442a3b1ffc9525f8c2f5c86
EC2 Default User <ec2-user@ip-172-31-11-98.eu-central-1.compute.internal>
2022-07-14 08:37:02 +0000

    Conflict resolved

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -32,11 +32,7 @@ module Api
       end
       private
       def recipe_params
-<<<<<<< HEAD
-        params.require(:recipe).permit(%i[name content price video_link])
-=======
-        params.require(:recipe).permit(%i[name content cooking_time price])
->>>>>>> main
+        params.require(:recipe).permit(%i[name content price video_link cooking_time])
       end
       def set_recipe
         @recipe = Recipe.find(params[:id])

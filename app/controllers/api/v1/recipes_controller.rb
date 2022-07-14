b65cc12898e9d3a2d3ca75b771ebc12c01883e33
Marcelina Dykas <dykasmarcelina@gmail.com>
2022-07-14 07:22:53 +0000

    Empty lines deleted and validation changed

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -2,35 +2,27 @@ module Api
   module V1
     class RecipesController < ApplicationController
       before_action :set_recipe, except: %i[create index]
-      
       def index
         render json: { data: ActiveModel::SerializableResource.new(Recipe.all, each_serializer: RecipeSerializer) }
       end
-
       def show
         render json: RecipeSerializer.new(@recipe).to_h
       end
-
       def create
         @recipe = Recipe.new(recipe_params)
-        
         if @recipe.save
           render json: @recipe
         else
           render json: { errors: @recipe.errors.to_s }, status: :unprocessable_entity
         end
-        
       end
-
       def update
-        
         if @recipe.update(recipe_params)
           render json: @recipe
         else
           render json: { errors: @recipe.errors.to_s }, status: :unprocessable_entity
         end
       end
-
       def destroy
         if @recipe.destroy
           render json: @recipe
@@ -38,13 +30,10 @@ module Api
           render json: { errors: @recipe.errors.to_s }, status: :unprocessable_entity
         end
       end
-
       private
-
       def recipe_params
         params.require(:recipe).permit(%i[name content cooking_time price])
       end
-
       def set_recipe
         @recipe = Recipe.find(params[:id])
       end
--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,12 +1,10 @@
 class RecipeSerializer < ActiveModel::Serializer
   attributes :id, :name, :content, :cooking_time, :price
-  
     def cooking_time
       return "unknown" if object.cooking_time.blank?
       
       object.cooking_time
     end
-      
     def price
         return "unknow" if object.price.blank?
         

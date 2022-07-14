4c90df0d9f0cf33e0b6689d77d0fa8ce481bd4d3
EC2 Default User <ec2-user@ip-172-31-13-137.eu-central-1.compute.internal>
2022-07-14 07:31:27 +0000

    Create commit

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,4 @@
 class Recipe < ApplicationRecord
   validates :name, presence: true
+  validates_numericality_of :level, if: Proc.new { |p| (p.level.is_a? Integer and p.level >= 1 and p.level <= 5) ? true : false }
 end
---
+++ db/migrate/20220712114718_add_level_field.rb
@@ -0,0 +1,5 @@
+class AddLevelField < ActiveRecord::Migration[6.1]
+  def change
+    add_column :recipes, :level, :integer
+  end
+end

0f0ab26d9498959d957ec0ea143001ef6e4beabc
EC2 Default User <ec2-user@ip-172-31-13-137.eu-central-1.compute.internal>
2022-07-15 10:54:05 +0000

    Changed visual changes

--- app/controllers/api/v1/rates_controller.rb
+++ app/controllers/api/v1/rates_controller.rb
@@ -2,10 +2,8 @@ module Api
   module V1
         class RatesController < ApplicationController
             def create
-                #z guide
                 @recipe = Recipe.find(params[:recipe_id])
                 @rate = @recipe.rates.create(rate_params)
-                #z recipes_controller
                 if @rate.save
                   render json: @recipe
                 else

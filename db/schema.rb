61168cd6d8b417af47222344ae87863776da66f4
EC2 Default User <ec2-user@ip-172-31-11-98.eu-central-1.compute.internal>
2022-07-18 11:44:28 +0000

    conflict resolved

--- db/schema.rb
+++ db/schema.rb
@@ -48,10 +48,6 @@ ActiveRecord::Schema.define(version: 2022_07_18_105220) do
     t.string "video_link"
     t.string "cooking_time"
     t.integer "level"
-<<<<<<< HEAD
-=======
-    t.string "video_link"
->>>>>>> origin/BE-08_Add_sign_in/sign_up
   end
 
   create_table "users", force: :cascade do |t|

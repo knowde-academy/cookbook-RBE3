b2928f130f239bb7441d3635cf648fdf2d2f45c4
EC2 Default User <ec2-user@ip-172-31-13-137.eu-central-1.compute.internal>
2022-07-19 12:10:03 +0000

    Update

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -2,5 +2,5 @@ class Rate < ApplicationRecord
   belongs_to :recipe
   belongs_to :user
   validates :rating, numericality: { only_integer: true, greater_than: 0, less_than: 6 }, allow_nil: true
-  validates :user_id, uniqueness: { scope: :recipe, message: "You already gave rate"}
+  validates :user_id, uniqueness: { scope: :recipe, message: "You already gave rate" }
 end

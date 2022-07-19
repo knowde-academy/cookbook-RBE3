a4e4428116eb3a53745e8a4111174785302c955e
EC2 Default User <ec2-user@ip-172-31-13-137.eu-central-1.compute.internal>
2022-07-19 12:36:58 +0000

    changes :)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
+  has_many :rates
   extend Devise::Models
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
